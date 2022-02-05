33471a2b3fd3260a5960569d12fecc529dd52d6e
Sven Fleshman <sven.fleshman@gmail.com>
2022-02-05 15:34:13 +0100

    build the navBarSections and fixed section-name for menu

--- js/app.js
+++ js/app.js
@@ -26,7 +26,10 @@
 //Get the button:
 const mybutton = document.getElementById("myButton");
 const scrollTopButton = document.querySelector(".myButtonClass");
-
+//Get the ul 
+const navBarList = document.querySelector('#navbar__list');
+//Get the sections 
+const sections = document.querySelectorAll('[data-nav]');
 
 /**
  * End Global Variables
@@ -34,6 +37,7 @@ const scrollTopButton = document.querySelector(".myButtonClass");
  * 
 */
 
+// build the Navigation auto sections
 
 /**
  * End Helper Functions
@@ -42,7 +46,27 @@ const scrollTopButton = document.querySelector(".myButtonClass");
 */
 
 // build the nav
-
+function navBarSections () {
+  sections.forEach((section, num) =>{
+    // declare the data-variables
+    let listItem = document.createElement('li');
+    let link = document.createElement('a');
+    let linkTitle = section.getAttribute('data-nav');
+    let linkTarget = section.getAttribute('id');
+    // one ul listItem per section gets the class list__item
+    listItem.classList.add('link_menu');
+    // ul gets the listItems as a child
+    navBarList.appendChild(listItem);
+    // listItem get a link as a child
+    listItem.appendChild(link);
+    // link gets the title text from data-nav
+    link.textContent = linkTitle;
+    //set attributes for target and add class to link
+    link.setAttribute("data-nav", `${linkTarget}`);
+    link.setAttribute("href", `#${linkTarget}`);
+    link.classList.add('blog_link_menu');
+  });
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -65,10 +89,9 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll smooth to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  window.scroll({top: 0, behavior: "smooth"});
 }
 
 /**
@@ -78,7 +101,7 @@ function topFunction() {
 */
 scrollTopButton.addEventListener("click", topFunction);
 // Build menu 
-
+document.addEventListener('DOMContentLoaded', navBarSections);
 // Scroll to section on link click
 
 // Set sections as active
